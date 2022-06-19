11fbca48c1e45ae8db7ce2504d4717ba7cbebd36
editor99 <yoonjaekim2010@gmail.com>
2022-06-19 17:06:16 +0900

    Update

--- main.sh
+++ main.sh
@@ -1,1 +1,14 @@
+# main.sh
+
+# when user executes command "cls" execute the command for clearing the screen,
+# depending on the distribution of the system
+if [ -z "$CLS" ]; then
+    if [ -x /usr/bin/clear ]; then
+        CLS="clear"
+    elif [ -x /bin/clear ]; then
+        CLS="clear"
+    else
+        CLS="echo"
+    fi
+fi
 
